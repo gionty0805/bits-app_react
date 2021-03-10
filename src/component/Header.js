9aca0e2fca775a552667791a8db429a68e37e693
김반석 <>
2021-03-10 17:40:06 +0900

    Header rendering

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+    
 const Header = () => (
-    <div className="header">
-        <div className="buttons">
-          <button><Link to= '/' className="links">Home</Link></button>
-          <button><Link to= '/Signpage' className="links">회원가입</Link></button>
-          <button><Link to= '/Login' className="links">로그인</Link></button>
-        </div>
-    </div>
+      <AppBar position="static">
+        <Toolbar>
+            <Button><Link to= '/' className="links">Home</Link></Button>
+            <Button><Link to= '/Signpage' className="links">회원가입</Link></Button>
+            <Button><Link to= '/Login' className="links">로그인</Link></Button>
+        </Toolbar>
+      </AppBar>
 );
 
 export default Header;
