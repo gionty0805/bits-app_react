816fb770d7a9151618d47c686d0facf4fd4efa38
김반석 <>
2021-03-11 14:10:09 +0900

    material test 완료

--- src/component/Header.js
+++ src/component/Header.js
@@ -8,14 +8,37 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
     
-const Header = () => (
-      <AppBar position="static">
-        <Toolbar>
-            <Button><Link to= '/' className="links">Home</Link></Button>
-            <Button><Link to= '/login' className="links">로그인</Link></Button>
-            <Button><Link to= '/join' className="links">회원가입</Link></Button>
-        </Toolbar>
-      </AppBar>
-);
-
-export default Header;
+const UseStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  }));
+export default function Header() {
+  const classes = UseStyles();
+  return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              News
+            </Typography>
+            <Toolbar>
+                <Button><Link to= '/' className="links">Home</Link></Button>
+                <Button><Link to= '/login' className="links">로그인</Link></Button>
+                <Button><Link to= '/join' className="links">회원가입</Link></Button>
+            </Toolbar>
+          </Toolbar>
+        </AppBar>
+      </div>
+      );
+};
+// export default Header;
